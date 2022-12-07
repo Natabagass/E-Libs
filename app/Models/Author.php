ba55f27118faea395b72fe264d80f3a33ba0aa9c
hilalfauzann <107779815+hilalfauzann@users.noreply.github.com>
2022-12-07 22:29:46 +0700

    menambahkan model author

---
+++ app/Models/Author.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Book;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'tanggal_lahir',
+        'alamat',
+    ];
+
+    
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+}
